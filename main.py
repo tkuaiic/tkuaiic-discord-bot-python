0794ba9cbbc785b17cef0eedb6092bf41d6d8cb9
Johnny Yeng <a2902793@gmail.com>
2022-11-04 23:02:24 +0800

    [Bug]Fixed Timeout by Adding Simple Message

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import interactions, os
-import firebase_admin
+import interactions, os, firebase_admin, logging
 from firebase_admin import credentials, firestore
 
 # These are basic inits for discord bot to function corrrectly
@@ -11,6 +10,12 @@ bot = interactions.Client(
 cred = credentials.Certificate("credentials.json") # <-Not for public
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+# These are basic inits for logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 #------------------------Helper Functions------------------------
 def check_df(collection, name):
     doc_ref = db.collection(collection).document(name)
@@ -44,20 +49,25 @@ async def verify(ctx):
 
 @bot.modal("verify_modal")
 async def modal_response(ctx, name: str, studentid: int):
+    discord_account = f'{ctx.author.username}#{ctx.author.discriminator}'
+    logging.info(f"Discord Account: {discord_account} | Name: {name} | StudentID: {studentid}")
     doc_ref = check_df(u"1111-member", name)
     entry = doc_ref.get()
     # Authentication logic(check if name and student ID matches the record in database)
     if entry.exists and entry.to_dict()['student_id'] == studentid:
+        await ctx.send(f"✅社員核對通過，請稍後...", ephemeral=True)
         # Add the user's discord account name into the database
         doc_ref.update({
-            u'discord_account': f'{ctx.author.username}#{ctx.author.discriminator}'
+            u'discord_account': discord_account
         })
+        logging.info(f" - Successfully Updated Discord Account for {name}")
         # Gets role ID
         roles, = interactions.search_iterable(ctx.guild.roles, name='第2屆社員 2nd Gen. Club Member')
         # Add role to user
         await ctx.author.add_role(roles.id)
+        logging.info(f" - Successfully Added Role to {name}")
         # Confirm
-        await ctx.channel.send(f"{name}您好，已將您加入`第2屆社員 2nd Gen. Club Member`身分組！", ephemeral=True)
+        await ctx.channel.send(f"{name}您好，已將您加入 `第2屆社員 2nd Gen. Club Member` 身分組！")
     else:
         # In case cadre needs to add member role
         doc_ref = check_df(u"1111-cadre", name)
@@ -68,6 +78,7 @@ async def modal_response(ctx, name: str, studentid: int):
                 u'discord_account': f'{ctx.author.username}#{ctx.author.discriminator}'
             })
             await ctx.send(f"已將您的Discord帳號登入至資料庫，謝謝！", ephemeral=True)
+            logging.info(" - Successfully Update Discord Account for {name}")
             return
         await ctx.send(f"驗證有誤，請確認姓名及學號是否正確。如有疑問，請透過<#1024724411074498591>頻道反應問題，謝謝！", ephemeral=True)
 
